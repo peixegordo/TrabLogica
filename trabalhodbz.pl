41f1af1834fbc2d8e687a630ac9d8f4fc1f230f8
Sauloferraz <32209343+Sauloferraz@users.noreply.github.com>
2017-09-22 19:32:51 -0300

    Add files via upload

---
+++ trabalhodbz.pl
@@ -0,0 +1,81 @@
+pai(bardock,raditz).
+pai(bardock,goku).
+pai(goku, gohan).
+pai(goku, goten).
+pai(reicutelo, chichi).
+pai(gohan, pan).
+pai(mrsatan, videl).
+pai(drbriefs, bulma).
+pai(drbriefs, tights).
+pai(reivegeta, vegeta).
+pai(reivegeta, tarble).
+pai(vegeta, trunks).
+pai(vegeta, bra).
+pai(bibidi, babidi).
+pai(rei cold, cooler).
+pai(reicold, freeza).
+pai(freeza, kuriza).
+pai(kuririn, marron).
+
+mae(gine, raditz).
+mae(gine, goku).
+mae(rainhacutelo, chichi).
+mae(chichi, gohan).
+mae(chichi, goten).
+mae(miguel, videl).
+mae(videl, pan).
+mae(srabriefs, bulma).
+mae(srabriefs,tights).
+mae(bulma, trunks).
+mae(bulma, bra).
+mae(rainhavegeta, vegeta).
+mae(rainhavegeta, tarble).
+mae(dezoito, marron).
+
+fusao(gotenks, goten).
+fusao(gotenks, trunks).
+fusao(vegetto, vegeta).
+fusao(vegetto, goku).
+fusao(gogeta,goku).
+fusao(gogeta,vegeta).
+
+avo(bardock, gohan).
+avo(bardock, goten).
+avo(reicutelo, gohan).
+avo(reicutelo, goten).
+avo(goku, pan).
+avo(mrsatan, pan).
+avo(drbriefs, trunks).
+avo(drbriefs, bra).
+avo(reivegeta, trunks).
+avo(reivegeta, bra).
+avo(reicold, kuriza).
+
+avo(gine, gohan).
+avo(gine, goten).
+avo(chichi, pan).
+avo(rainhacutelo, pan).
+avo(miguel, pan).
+avo(srabriefs, trunks).
+avo(srabriefs, bra).
+avo(rainhavegeta, trunks).
+avo(rainhavegeta, bra).
+
+casados(bardock, gine).
+casados(goku, chichi).
+casados(reicutelo, rainhacutelo).
+casados(gohan, videl).
+casados(mrsatan, miguel).
+casados(drbriefs,srabriefs).
+casados(vegeta, bulma).
+casados(reivegeta, rainhavegeta).
+casados(kuririn, dezoito).
+
+tio(raditz, gohan).
+tio(raditz, goten).
+tio(goten, pan).
+tio(tights, trunks).
+tio(tights, bra).
+tio(tarble, trunks).
+tio(tarble, bra).
+
